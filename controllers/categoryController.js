dd9960992ea29eb2337e721f59791c8ef8188057
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-27 16:31:47 +0200

    Feat: Read and Create new category from DB

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,8 +1,56 @@
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
   const categories = await Category.find({}, "name").sort({ name: 1 }).exec();
 
   res.render("index", { title: "Keyboard store", category_list: categories });
 });
+
+exports.category_create_get = (req, res, next) => {
+  res.render("category_create", { title: "Create category" });
+};
+
+exports.category_create_post = [
+  body("name", "Category must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body(
+    "description",
+    "Category description must contain at least 10 characters"
+  )
+    .trim()
+    .isLength({ min: 10 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_create", {
+        title: "Create category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const categoryExists = await Category.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (categoryExists) {
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
