3ca4e0406dfb8c6f5ae1a5910e2c7786b7ae3c57
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-27 19:09:08 +0200

    Feat: route and query each item + details
    * In order to link to specific item pages they need their own page +
    information of that item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const category = require("../models/category");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const items = await Item.find({ category: req.params.id }).exec();
@@ -9,6 +10,18 @@ exports.index = asyncHandler(async (req, res, next) => {
   res.render("category", {
     itemList: items,
     title: category.name,
+    category: category,
+  });
+});
+
+exports.item_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).exec();
+  const category = await Category.findById(item.category.toString(), "name");
+
+  res.render("item.pug", {
+    title: item.name,
+    item: item,
+    categoryID: item.category.toString(),
     category: category,
   });
 });
--- routes/items.js
+++ routes/items.js
@@ -5,4 +5,6 @@ const item_controller = require("../controllers/itemController");
 
 router.get("/:id", item_controller.index);
 
+router.get("/item/:id", item_controller.item_get);
+
 module.exports = router;
