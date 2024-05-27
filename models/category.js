57edf7c8883fedbf0ff0790e8607483db76c63c9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-27 16:30:55 +0200

    Fix: set minLength to prevent empty fields

--- models/category.js
+++ models/category.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-  name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, required: true, maxLength: 1000 },
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+  description: { type: String, required: true, minLength: 10, maxLength: 1000 },
 });
 
 CategorySchema.virtual("url").get(function () {
