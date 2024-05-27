4e9acce96ec403025be0332f4ac73d11146505b7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-27 15:16:15 +0200

    Feat: rename users to items
    * Users router does not apply to project, items suits it more

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const itemsRouter = require("./routes/items");
 const { mainModule } = require("process");
 const app = express();
 
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/category", itemsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
