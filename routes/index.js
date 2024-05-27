16e96970378475fe97cacc0a9d2d757beea6c9f9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-27 17:47:24 +0200

    Feat: route to correct delete page from category

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,6 @@ router.get("/", category_controller.category_list);
 router.get("/create", category_controller.category_create_get);
 router.post("/create", category_controller.category_create_post);
 
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
 module.exports = router;
