dbcb4254b15403004d44711730293f9de2503f3f
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-28 14:03:04 +0200

    Feat: handle item update form for GET+POST

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -113,3 +113,72 @@ exports.item_create_post = [
     }
   }),
 ];
+
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  const [item, allCategories] = await Promise.all([
+    Item.findById(req.params.id).exec(),
+    Category.find({}, "name").sort({ name: 1 }).exec(),
+  ]);
+  const category = await Category.findById(
+    item.category.toString(),
+    "name"
+  ).exec();
+
+  if (item === null) {
+    const err = new Error("Item not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("item_create", {
+    title: "Update",
+    item: item,
+    categories: allCategories,
+    item_category: category,
+  });
+});
+
+exports.item_update_post = [
+  body("name", "Item name must not be empty and have at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body(
+    "description",
+    "Item description must not be empty and have at least 10 characters"
+  )
+    .trim()
+    .isLength({ min: 10 })
+    .escape(),
+  body("category", "Category selection is required").trim().escape(),
+  body("price", "Price must not be empty").trim().isFloat().escape(),
+  body("numberInStock", "Number in stock must not be empty")
+    .trim()
+    .isInt()
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      numberInStock: req.body.numberInStock,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("item_create", {
+        title: "Update",
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedItem = await Item.findByIdAndUpdate(req.params.id, item);
+      res.redirect("/category" + item.url);
+    }
+  }),
+];
