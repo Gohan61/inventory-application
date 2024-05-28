5a257eba23473fabbb1a7f0bd13df481d503c02f
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-28 10:17:41 +0200

    Fix: change route order to prevent :id prevailing
    * In order to match some routes, :id prefixed routes need to be in the
    end or they will always match first

--- routes/items.js
+++ routes/items.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const item_controller = require("../controllers/itemController");
 
-router.get("/:id", item_controller.index);
-router.get("/item/:id", item_controller.item_get);
+router.get("/item/create", item_controller.item_create_get);
+router.post("item/create", item_controller.item_create_post);
 
 router.get("/item/:id/delete", item_controller.item_delete_get);
 router.post("/item/:id/delete", item_controller.item_delete_post);
 
-router.get("item/:id/create", item_controller.item_create_get);
-router.post("item/:id/create", item_controller.item_create_post);
+router.get("/item/:id", item_controller.item_get);
+router.get("/:id", item_controller.index);
 
 module.exports = router;
