2e9ed31beca2fd6af0feb4523599ff1f9795d1d3
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-28 11:25:30 +0200

    Fix: prefix route with / to prevent failure

--- routes/items.js
+++ routes/items.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const item_controller = require("../controllers/itemController");
 
 router.get("/item/create", item_controller.item_create_get);
-router.post("item/create", item_controller.item_create_post);
+router.post("/item/create", item_controller.item_create_post);
 
 router.get("/item/:id/delete", item_controller.item_delete_get);
 router.post("/item/:id/delete", item_controller.item_delete_post);
