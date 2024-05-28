5f24dcf6296f36eab47da8287f302ef523aec8a2
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-28 06:53:57 +0200

    Feat: define routes for GET+POST delete of item

--- routes/items.js
+++ routes/items.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const item_controller = require("../controllers/itemController");
 
 router.get("/:id", item_controller.index);
-
 router.get("/item/:id", item_controller.item_get);
+
+router.get("/item/:id/delete", item_controller.item_delete_get);
+router.post("/item/:id/delete", item_controller.item_delete_post);
 
 module.exports = router;
