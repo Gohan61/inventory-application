14ae5a1093c33a0795226a6368f52804f0557418
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-28 11:20:19 +0200

    Feat: define minLength on name+description
    * Added a minLength property to prevent single character like entries

--- models/item.js
+++ models/item.js
@@ -4,8 +4,8 @@ const category = require("./category");
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, required: true, maxLength: 1000 },
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+  description: { type: String, required: true, minLength: 10, maxLength: 1000 },
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   price: { type: Schema.Types.Decimal128, required: true },
   numberInStock: { type: Number, required: true },
