0b3b8d43e718d408ec4647c28967964817826d7b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-28 07:17:54 +0200

    Feat: define routes for new item creation

--- routes/items.js
+++ routes/items.js
@@ -9,4 +9,7 @@ router.get("/item/:id", item_controller.item_get);
 router.get("/item/:id/delete", item_controller.item_delete_get);
 router.post("/item/:id/delete", item_controller.item_delete_post);
 
+router.get("item/:id/create", item_controller.item_create_get);
+router.post("item/:id/create", item_controller.item_create_post);
+
 module.exports = router;
